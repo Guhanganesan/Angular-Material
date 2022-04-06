9e3247d6cbb5c1de2fdc3f91282c1bff27ada448
guhanganesan <raghavaguhan@gmail.com>
2022-04-06 23:29:59 +0530

    Reactive Form Example

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 //Components
 import { EmployeeComponent } from './employee/employee.component';
 
--- myapp/src/app/student/student.component.ts
+++ myapp/src/app/student/student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-student',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+  student_form!: FormGroup;
 
   ngOnInit(): void {
+    this.student_form = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      nick_name: ['', Validators.required],
+      family_name: ['', Validators.required],
+      address: ['', Validators.required],
+      district: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.first_name);
+    console.log('Email', form.value.last_name);
+    console.log('Message', form.value.nick_name);
+    console.log('Name', form.value.family_name);
+    console.log('Email', form.value.address);
+    console.log('Message', form.value.email);
+    console.log('Message', form.value.mobile);
+    console.log('Email', form.value.username);
+    console.log('Message', form.value.password);
   }
 
 }
