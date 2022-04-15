3c006701aedc17e6b8ddbd257073b0cc935d90c0
guhanganesan <raghavaguhan@gmail.com>
2022-04-15 16:25:34 +0530

    @Viewchild() example for accessing child components properties in parent component

--- myapp/src/app/decorators-examples/manager/manager-details/manager-details.component.ts
+++ myapp/src/app/decorators-examples/manager/manager-details/manager-details.component.ts
@@ -9,11 +9,26 @@ export class ManagerDetailsComponent implements OnInit {
 
   @Input() manager_details:any;
 
+  area_name:string;
+  pin_code:number;
+
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.manager_details.name)
+    // console.log(this.manager_details.age)
+    this.childDetails();
+  }
+
+  showDetails(){
     console.log(this.manager_details.name)
     console.log(this.manager_details.age)
+    console.log(this.manager_details.mobile)
+  }
+
+  childDetails(){
+    this.area_name = "Ambedkar Street";
+    this.pin_code =  777777;
   }
 
 }
--- myapp/src/app/decorators-examples/manager/manager.component.ts
+++ myapp/src/app/decorators-examples/manager/manager.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ManagerDetailsComponent } from './manager-details/manager-details.component';
 
 @Component({
   selector: 'app-manager',
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
-export class ManagerComponent implements OnInit {
+export class ManagerComponent implements OnInit, AfterViewInit {
 
   manager = {
-    name:'Guhan',
+    name:'Guhan Ganesan',
     age:31,
-    mobile:878937
+    mobile:88888
   }
+
+  @ViewChild(ManagerDetailsComponent, {static:true}) child_comp: ManagerDetailsComponent;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    // call child component's method
+    this.child_comp.showDetails();
+    // get child component's properties
+    console.log(this.child_comp.area_name);
+    console.log(this.child_comp.pin_code)
   }
 
 }
