64da2ed6d1d339b590ccb696e90064f336e9df3f
guhanganesan <raghavaguhan@gmail.com>
2022-04-15 17:15:39 +0530

    @ViewChildern() example using QueryList<T> for multiple values

--- myapp/src/app/decorators-examples/manager/manager.component.ts
+++ myapp/src/app/decorators-examples/manager/manager.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { ManagerDetailsComponent } from './manager-details/manager-details.component';
 
 @Component({
@@ -8,13 +8,31 @@ import { ManagerDetailsComponent } from './manager-details/manager-details.compo
 })
 export class ManagerComponent implements OnInit, AfterViewInit {
 
-  manager = {
+//   manager = {name:'Guhan Ganesan',
+//   age:31,
+//   mobile:88888
+// }
+
+  manager_data = [{
     name:'Guhan Ganesan',
     age:31,
     mobile:88888
+  },
+  {
+    name:'Anbarasan Ganesan',
+    age:28,
+    mobile:84493
+  },
+  {
+    name:'Arivu Ganesan',
+    age:35,
+    mobile:34874
   }
+]
 
   @ViewChild(ManagerDetailsComponent, {static:true}) child_comp: ManagerDetailsComponent;
+
+  @ViewChildren(ManagerDetailsComponent) child_comp_list: QueryList<ManagerDetailsComponent>;
 
   constructor() { }
 
@@ -23,10 +41,13 @@ export class ManagerComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(){
     // call child component's method
-    this.child_comp.showDetails();
+    //this.child_comp.showDetails();
     // get child component's properties
-    console.log(this.child_comp.area_name);
-    console.log(this.child_comp.pin_code)
+    //console.log(this.child_comp.area_name);
+    //console.log(this.child_comp.pin_code)
+    this.child_comp_list.toArray().forEach((item)=>{
+      console.log(item.manager_details)
+    })
   }
-
+  
 }
